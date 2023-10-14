dd96c017ad29d07bcec2914b6424e425248e451e
GAUTAM KUMAR <gkvc9696@gmail.com>
2023-10-14 17:01:48 +0530

    added:pages

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import { Expenses, Home, Reports, Saving } from "./pages";
 
 function App() {
-  return (
-    <div className="App">
-     <h1>Hello</h1>
-    </div>
-  );
+	return (
+		<div className="App">
+			<Routes>
+				<Route path="/" element={<Home />} />
+				<Route path="/expenses" element={<Expenses />} />
+				<Route path="/saving" element={<Saving />} />
+				<Route path="/reports" element={<Reports />} />
+			</Routes>
+		</div>
+	);
 }
 
 export default App;
